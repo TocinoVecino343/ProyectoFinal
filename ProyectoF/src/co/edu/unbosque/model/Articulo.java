424b8fdfe5ad1b3afe5fa43cd7337a95454cbf76
User <User@Administrator>
2025-08-13 22:48:50 -0500

    Articulo

---
+++ ProyectoF/src/co/edu/unbosque/model/Articulo.java
@@ -0,0 +1,75 @@
+package co.edu.unbosque.model;
+
+public class Articulo extends Publicacion {
+
+	private String autor;
+	private String fuente;
+	private String terminosGeograficos;
+	private int cantidadDePaginas;
+
+	public Articulo() {
+		super();
+	}
+
+	public Articulo(String autor, String fuente, String terminosGeograficos, int cantidadDePaginas) {
+		super();
+		this.autor = autor;
+		this.fuente = fuente;
+		this.terminosGeograficos = terminosGeograficos;
+		this.cantidadDePaginas = cantidadDePaginas;
+	}
+
+	public Articulo(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion, String idioma,
+			String autor, String fuente, String terminosGeograficos, int cantidadDePaginas) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma);
+		this.autor = autor;
+		this.fuente = fuente;
+		this.terminosGeograficos = terminosGeograficos;
+		this.cantidadDePaginas = cantidadDePaginas;
+	}
+
+	public Articulo(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getFuente() {
+		return fuente;
+	}
+
+	public void setFuente(String fuente) {
+		this.fuente = fuente;
+	}
+
+	public String getTerminosGeograficos() {
+		return terminosGeograficos;
+	}
+
+	public void setTerminosGeograficos(String terminosGeograficos) {
+		this.terminosGeograficos = terminosGeograficos;
+	}
+
+	public int getCantidadDePaginas() {
+		return cantidadDePaginas;
+	}
+
+	public void setCantidadDePaginas(int cantidadDePaginas) {
+		this.cantidadDePaginas = cantidadDePaginas;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + " Articulo [autor=" + autor + ", fuente=" + fuente + ", terminosGeograficos="
+				+ terminosGeograficos + ", cantidadDePaginas=" + cantidadDePaginas + "]";
+	}
+
+}
