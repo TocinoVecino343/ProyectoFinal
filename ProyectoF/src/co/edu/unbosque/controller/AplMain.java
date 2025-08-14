a91b91ea58e08135db6dc292ee2015c3d3987f22
nicob <nicob@LAPTOP-6HKONR69>
2025-08-13 21:39:52 -0500

    Mergeu branch 'master' of https://github.com/TocinoVecino343/ProyectoFinal.git

--- ProyectoF/src/co/edu/unbosque/controller/AplMain.java
+++ ProyectoF/src/co/edu/unbosque/controller/AplMain.java
@@ -1,9 +1,12 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.controlador.Controlador;
+
 public class AplMain {
 
 	public static void main(String[] args) {
-	
+		Controlador c = new Controlador();
+		c.run();
 	}
 
 }
