91b55f8d72dc11d24f45f29f64c0b22775c16054
sanchezgina0312 <sanchez.gina0312@gmail.com>
2025-08-13 22:49:07 -0500

    Cambios en los constructores de los libros, añadi los 8

--- ProyectoF/src/co/edu/unbosque/model/LibroFisico.java
+++ ProyectoF/src/co/edu/unbosque/model/LibroFisico.java
@@ -1,37 +1,83 @@
 package co.edu.unbosque.model;
 
-public final class LibroFisico {
+public final class LibroFisico extends Libro {
 
 	private int cantidadDeCopias;
 	private int tiempoDePrestamo;
 	
+	//Constructores (8)
 	
+	public LibroFisico() {
+		// TODO Auto-generated constructor stub
+	}
 	
-	//Constructores,son 8
+	public LibroFisico(int cantidadDeCopias, int tiempoDePrestamo) {
+		super();
+		this.cantidadDeCopias = cantidadDeCopias;
+		this.tiempoDePrestamo = tiempoDePrestamo;
+	}
+
+	public LibroFisico(int cantidadDePaginas, String autor, String editorial, int cantidadDeCopias,
+			int tiempoDePrestamo) {
+		super(cantidadDePaginas, autor, editorial);
+		this.cantidadDeCopias = cantidadDeCopias;
+		this.tiempoDePrestamo = tiempoDePrestamo;
+	}
+
+	public LibroFisico(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma, int cantidadDePaginas, String autor, String editorial, int cantidadDeCopias,
+			int tiempoDePrestamo) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma, cantidadDePaginas, autor, editorial);
+		this.cantidadDeCopias = cantidadDeCopias;
+		this.tiempoDePrestamo = tiempoDePrestamo;
+	}
 	
+	public LibroFisico(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma, int cantidadDeCopias, int tiempoDePrestamo) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma);
+		this.cantidadDeCopias = cantidadDeCopias;
+		this.tiempoDePrestamo = tiempoDePrestamo;
+	}
 	
+	public LibroFisico(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma, int cantidadDePaginas, String autor, String editorial) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma, cantidadDePaginas, autor, editorial);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LibroFisico(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LibroFisico(int cantidadDePaginas, String autor, String editorial) {
+		super(cantidadDePaginas, autor, editorial);
+		// TODO Auto-generated constructor stub
+	}
 	
-	
-	
+	// getters y setters
+
 	public int getCantidadDeCopias() {
 		return cantidadDeCopias;
 	}
+	
 	public void setCantidadDeCopias(int cantidadDeCopias) {
 		this.cantidadDeCopias = cantidadDeCopias;
 	}
+	
 	public int getTiempoDePrestamo() {
 		return tiempoDePrestamo;
 	}
+	
 	public void setTiempoDePrestamo(int tiempoDePrestamo) {
 		this.tiempoDePrestamo = tiempoDePrestamo;
 	}
+	
 	@Override
 	public String toString() {
 		return super.toString()+" LibroFisico [cantidadDeCopias=" + cantidadDeCopias + ", tiempoDePrestamo=" + tiempoDePrestamo + "]";
 	}
-	
-	
-	
 	
 	
 	
--- ProyectoF/src/co/edu/unbosque/model/LibroVirtual.java
+++ ProyectoF/src/co/edu/unbosque/model/LibroVirtual.java
@@ -1,23 +1,79 @@
 package co.edu.unbosque.model;
 
-public class LibroVirtual {
+public class LibroVirtual extends Libro {
 
-	//8 constructores
-	
 	private String tipoDeArchivo;
 	private boolean esAudioLibro;
+	
+	// Constructores (8)
+	
+	public LibroVirtual() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public LibroVirtual(String tipoDeArchivo, boolean esAudioLibro) {
+		super();
+		this.tipoDeArchivo = tipoDeArchivo;
+		this.esAudioLibro = esAudioLibro;
+	}
+
+	public LibroVirtual(int cantidadDePaginas, String autor, String editorial, String tipoDeArchivo,
+			boolean esAudioLibro) {
+		super(cantidadDePaginas, autor, editorial);
+		this.tipoDeArchivo = tipoDeArchivo;
+		this.esAudioLibro = esAudioLibro;
+	}
+	
+	public LibroVirtual(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma, int cantidadDePaginas, String autor, String editorial, String tipoDeArchivo,
+			boolean esAudioLibro) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma, cantidadDePaginas, autor, editorial);
+		this.tipoDeArchivo = tipoDeArchivo;
+		this.esAudioLibro = esAudioLibro;
+	}
+	
+	public LibroVirtual(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma, String tipoDeArchivo, boolean esAudioLibro) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma);
+		this.tipoDeArchivo = tipoDeArchivo;
+		this.esAudioLibro = esAudioLibro;
+	}
+
+	public LibroVirtual(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma, int cantidadDePaginas, String autor, String editorial) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma, cantidadDePaginas, autor, editorial);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LibroVirtual(int id, String titulo, String clasificacion, String facultad, int fechaDePublicacion,
+			String idioma) {
+		super(id, titulo, clasificacion, facultad, fechaDePublicacion, idioma);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LibroVirtual(int cantidadDePaginas, String autor, String editorial) {
+		super(cantidadDePaginas, autor, editorial);
+		// TODO Auto-generated constructor stub
+	}
+
+	// getters y setters
+	
 	public String getTipoDeArchivo() {
 		return tipoDeArchivo;
 	}
+	
 	public void setTipoDeArchivo(String tipoDeArchivo) {
 		this.tipoDeArchivo = tipoDeArchivo;
 	}
+	
 	public boolean isEsAudioLibro() {
 		return esAudioLibro;
 	}
+	
 	public void setEsAudioLibro(boolean esAudioLibro) {
 		this.esAudioLibro = esAudioLibro;
 	}
+	
 	@Override
 	public String toString() {
 		return super.toString()+" LibroVirtual [tipoDeArchivo=" + tipoDeArchivo + ", esAudioLibro=" + esAudioLibro + "]";
