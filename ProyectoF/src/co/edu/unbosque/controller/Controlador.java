20cfda261422a5d553ae3eb4b7251e737923c138
User <User@Administrator>
2025-08-15 21:17:18 -0500

    Controlador logica

--- ProyectoF/src/co/edu/unbosque/controller/Controlador.java
+++ ProyectoF/src/co/edu/unbosque/controller/Controlador.java
@@ -1,109 +1,39 @@
 package co.edu.unbosque.controller;
 
 import java.util.ArrayList;
-import co.edu.unbosque.model.*;
+import co.edu.unbosque.model.persistence.*;
+import co.edu.unbosque.view.*;
 
 public class Controlador {
 
-	// hola
-
-	private LibroFisico objf;
-	private LibroVirtual objLv;
-	private Steam objS;
-	private Revista objR;
-	private Pelicula objP;
-	private Articulo objA;
-
-	private ArrayList<LibroFisico> listaLibrosFisicos;
-	private ArrayList<LibroVirtual> listaLibroVirtuales;
-	private ArrayList<Steam> listaSteam;
-	private ArrayList<Revista> listaRevistas;
-	private ArrayList<Pelicula> listaPeliculas;
-	private ArrayList<Articulo> listaArticulos;
-
-	public void run() {
-
-	}
+	private LibroFisicoDAO objf;
+	private LibroVirtualDAO objLv;
+	private SteamDAO objS;
+	private RevistaDAO objR;
+	private PeliculaDAO objP;
+	private ArticuloDAO objA;
+	private Consola con;
 
 	public Controlador() {
-		objf = new LibroFisico();
-		objLv = new LibroVirtual();
-		objS = new Steam();
-		objR = new Revista();
-		objP = new Pelicula();
-		objA = new Articulo();
-		
-		listaLibrosFisicos = new ArrayList<>();
-		listaLibroVirtuales = new ArrayList<>();
-		listaSteam = new ArrayList<>();
-		listaRevistas = new ArrayList<>();
-		listaPeliculas = new ArrayList<>();
-		listaArticulos = new ArrayList<>();
-
+		objf = new LibroFisicoDAO();
+		objLv = new LibroVirtualDAO();
+		objS = new SteamDAO();
+		objR = new RevistaDAO();
+		objP = new PeliculaDAO();
+		objA = new ArticuloDAO();
+		con = new Consola();
 	}
 
-	public LibroFisico getObjf() {
-		return objf;
+	public void run() {
+		int opcion = 0;
+		do {
+			con.escribirConSalto("Bienvenido a la biblioteca de la UEB");
+			con.escribirConSalto("Seleccione la opcion que desea realizar");
+			
+			con.escribirConSalto("1. Crear Publicacion"
+					+ "\n2. Mostrar Publicacion"
+					+ "\n3. Eliminar Publicacion"
+					+ "\n4. Actualizar Publicacion ");
+		} while (opcion != 8);
 	}
-
-	public void setObjf(LibroFisico objf) {
-		this.objf = objf;
-	}
-
-	public LibroVirtual getObjLv() {
-		return objLv;
-	}
-
-	public void setObjLv(LibroVirtual objLv) {
-		this.objLv = objLv;
-	}
-
-	public Revista getObjR() {
-		return objR;
-	}
-
-	public void setObjR(Revista objR) {
-		this.objR = objR;
-	}
-
-	public Pelicula getObjP() {
-		return objP;
-	}
-
-	public void setObjP(Pelicula objP) {
-		this.objP = objP;
-	}
-
-	public ArrayList<LibroFisico> getListaLibrosFisicos() {
-		return listaLibrosFisicos;
-	}
-
-	public void setListaLibrosFisicos(ArrayList<LibroFisico> listaLibrosFisicos) {
-		this.listaLibrosFisicos = listaLibrosFisicos;
-	}
-
-	public ArrayList<LibroVirtual> getListaLibroVirtuales() {
-		return listaLibroVirtuales;
-	}
-
-	public void setListaLibroVirtuales(ArrayList<LibroVirtual> listaLibroVirtuales) {
-		this.listaLibroVirtuales = listaLibroVirtuales;
-	}
-
-	public ArrayList<Revista> getListaRevistas() {
-		return listaRevistas;
-	}
-
-	public void setListaRevistas(ArrayList<Revista> listaRevistas) {
-		this.listaRevistas = listaRevistas;
-	}
-
-	public ArrayList<Pelicula> getListaPeliculas() {
-		return listaPeliculas;
-	}
-
-	public void setListaPeliculas(ArrayList<Pelicula> listaPeliculas) {
-		this.listaPeliculas = listaPeliculas;
-	}
-
 }
