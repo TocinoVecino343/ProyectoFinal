fabb94bbddc9d9904fcedd0ca5c37e5523fb2470
sanchezgina0312 <sanchez.gina0312@gmail.com>
2025-08-18 21:12:56 -0500

    Quite el c.run del AplMain

--- ProyectoF/src/co/edu/unbosque/controller/AplMain.java
+++ ProyectoF/src/co/edu/unbosque/controller/AplMain.java
@@ -4,7 +4,6 @@ public class AplMain {
 
 	public static void main(String[] args) {
 		Controlador c = new Controlador();
-		c.run();
 		c.runGUI(); //Corre la ventana principal cuando el usuario selecciona SALIR
 		
 	}
