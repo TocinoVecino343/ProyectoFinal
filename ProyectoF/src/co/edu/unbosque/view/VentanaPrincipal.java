93812a46036415008ad20ca855b638b1f2113a9c
nicob <nicob@LAPTOP-6HKONR69>
2025-08-19 22:14:58 -0500

    hola

--- ProyectoF/src/co/edu/unbosque/view/VentanaPrincipal.java
+++ ProyectoF/src/co/edu/unbosque/view/VentanaPrincipal.java
@@ -18,6 +18,7 @@ public class VentanaPrincipal extends JFrame {
 	private JButton botonMostrarPublicacion;
 	private JButton botonEliminarPublicacion;
 	private JButton botonActualizarPublicacion;
+	private JButton botonSalir;
 	private JLabel mensajeBienvenida;
 
 	public VentanaPrincipal() {
@@ -72,7 +73,13 @@ public class VentanaPrincipal extends JFrame {
 		this.add(botonActualizarPublicacion);
 		this.setLayout(null);
 		//
-		
+		botonSalir = new JButton("Salir");
+		botonSalir.setBounds(290, 500, 200, 30);
+		botonSalir.setBackground(Color.RED);
+		botonSalir.setForeground(Color.WHITE);
+		botonSalir.setFocusPainted(false);
+		this.add(botonSalir);
+		this.setLayout(null);
 
 	}
 
@@ -107,6 +114,14 @@ public class VentanaPrincipal extends JFrame {
 
 	public void setBotonActualizarPublicacion(JButton botonActualizarPublicacion) {
 		this.botonActualizarPublicacion = botonActualizarPublicacion;
+	}
+
+	public JButton getBotonSalir() {
+		return botonSalir;
+	}
+
+	public void setBotonSalir(JButton botonSalir) {
+		this.botonSalir = botonSalir;
 	}
 
 	public JLabel getMensajeBienvenida() {
