59412968fc50a826c266ae8ba11950ae7402d7bd
sanchezgina0312 <sanchez.gina0312@gmail.com>
2025-08-22 01:25:27 -0500

    Cambio do nothing on cloe a dispose on close en ventanamostrartodo

--- ProyectoF/src/co/edu/unbosque/view/VentanaMostrarTodo.java
+++ ProyectoF/src/co/edu/unbosque/view/VentanaMostrarTodo.java
@@ -17,7 +17,7 @@ public class VentanaMostrarTodo extends JFrame {
 		this.setTitle("Mostrar todo");
 		this.setBounds(50, 50, 1280, 800);
 		this.setResizable(false);
-		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setLayout(null);
 
 		textoMostrarTodo = new JTextArea();
